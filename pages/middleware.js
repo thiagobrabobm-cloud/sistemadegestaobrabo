d4cc7151df41f36dd2e5181e6695df453cfe83c9
Thiago Brabo <thiagobrabobm@gmail.com>
2025-08-27 11:31:08 -0300

    Corrigindo middleware para NextAuth

--- pages/middleware.js
+++ pages/middleware.js
@@ -1,7 +1,6 @@
 // middleware.js
 export { default } from "next-auth/middleware";
 
-// Define quais rotas devem exigir login
 export const config = {
-  matcher: ["/sgb.html", "/dashboard"],
+  matcher: ["/dashboard", "/sgb.html"],
 };
