d999a368cff2d975d123900eba9ab2d1cfd6d376
Thiago Brabo <thiagobrabobm@gmail.com>
2025-08-27 11:11:22 -0300

    Corrigindo exportação de authOptions

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+export const authOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
-  debug: true, // mostra logs úteis no terminal
+  debug: true, // mostra logs úteis
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -14,9 +14,15 @@ export default NextAuth({
   callbacks: {
     async signIn({ user }) {
       // só estes 2 e-mails podem entrar
-      const allowed = new Set(["thiagobrabobm@gmail.com", "marlucezanco02@gmail.com"]);
+      const allowed = new Set([
+        "thiagobrabobm@gmail.com",
+        "marlucezanco02@gmail.com"
+      ]);
       const email = (user?.email || "").toLowerCase().trim();
       return allowed.has(email);
     },
   },
-});
+};
+
+// exporta para o NextAuth
+export default NextAuth(authOptions);
